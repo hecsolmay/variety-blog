ce6d8d08cdb026872b4e3654e47e2cc7ffdd2ccb
hecsolmay <hecsolmay@gmail.com>
2024-06-11 16:47:20 -0600

    add eslint tailwindcss plugin

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -14,7 +14,7 @@ export default function Input (
         className={cn(
           `h-9 w-full appearance-none rounded-md border border-contrast bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 
           file:bg-transparent file:text-sm file:font-medium 
-          focus:outline-contrast focus:ring-accent focus-visible:outline-accent focus-visible:ring-1 
+          focus:outline-primary focus:ring-accent focus-visible:outline-primary focus-visible:ring-1 
           disabled:cursor-not-allowed disabled:opacity-50`,
           hasError && 'animate-error-shake border-red-500 focus:outline-red-500 focus:ring-red-500 focus-visible:outline-red-500 focus-visible:ring-red-500',
           className
