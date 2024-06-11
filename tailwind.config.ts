76bc98f2606b63f9ab068756a20f794750fddb40
hecsolmay <hecsolmay@gmail.com>
2024-06-11 17:35:04 -0600

    add moveup animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,10 +22,15 @@ const config: Config = {
           '0%, 100%': { transform: 'translateX(0)' },
           '10%, 30%, 50%, 70%, 90%': { transform: 'translateX(-1px)' },
           '20%, 40%, 60%, 80%': { transform: 'translateX(1px)' }
+        },
+        'moveUp': {
+          '0%': { transform: 'translateY(10px)' },
+          '100%': { transform: 'translateY(0)' }
         }
       },
       animation: {
-        'error-shake': 'shake 1s linear ease-in-out'
+        'error-shake': 'shake 1s linear ease-in-out',
+        'moveUp': 'moveUp 0.15s ease-in-out'
       }
     }
   },
