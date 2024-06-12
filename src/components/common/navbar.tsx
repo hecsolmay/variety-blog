1546e7aa81bce2156d382a664100908c966c4f62
hecsolmay <hecsolmay@gmail.com>
2024-06-11 19:22:47 -0600

    add navbar animation on scroll

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -6,7 +6,7 @@ import { Suspense } from 'react'
 
 export default function Navbar () {
   return (
-    <header className='sticky left-0 top-0 z-10 flex h-20 w-full items-center justify-between bg-white px-4'>
+    <header className='nav-scroll-shadow sticky left-0 top-0 z-10 flex h-16 w-full items-center justify-between bg-white px-4'>
       <Link href='/' className='inline-flex items-center'>
         <LogoIcon className='h-8 w-auto' />
         <span className='mt-1 hidden text-2xl font-semibold text-primary md:block'>
