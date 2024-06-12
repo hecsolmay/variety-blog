ab28202876836b44dd3456972b2cc431200415de
hecsolmay <hecsolmay@gmail.com>
2024-06-11 18:01:31 -0600

    add navbar and footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Footer from '@/components/common/footer'
+import Navbar from '@/components/common/navbar'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -6,23 +8,28 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Variety Blog',
-  description: 'El blog donde compartamos lo que nos interesa y lo que nos gusta, mostrando la variedad de cosas que nos rodean.'
+  description:
+    'El blog donde compartamos lo que nos interesa y lo que nos gusta, mostrando la variedad de cosas que nos rodean.'
 }
 
 export default function RootLayout ({
   children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <link
-        rel="icon"
-        href="/icon?<generated>"
-        type="image/<generated>"
-        sizes="<generated>"
+        rel='icon'
+        href='/icon?<generated>'
+        type='image/<generated>'
+        sizes='<generated>'
       />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
