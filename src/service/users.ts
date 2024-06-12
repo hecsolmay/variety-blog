ddc5d49a0674a9b2378e0813414462e1e5348f7f
hecsolmay <hecsolmay@gmail.com>
2024-06-12 13:37:03 -0600

    add getAuthors Function

--- src/service/users.ts
+++ src/service/users.ts
@@ -1,6 +1,8 @@
+import { SearchParams } from '@/types/props'
+import { formatPagination } from '@/utils/format'
 import prisma from '@/utils/prisma'
 
-export async function getUser (id: string)  {
+export async function getUser (id: string) {
   const user = await prisma.user.findFirst({
     where: {
       id
@@ -8,4 +10,36 @@ export async function getUser (id: string)  {
   })
 
   return user
+}
+
+export async function getAuthors (searchParams: SearchParams) {
+  const rawPagination = {
+    page: searchParams.page ?? 1,
+    limit: searchParams.limit ?? 12
+  }
+  const pagination = formatPagination(rawPagination)
+
+  const authorsPromise = prisma.user.findMany({
+    where: { posts: { some: {} } },
+    skip: pagination.skip,
+    take: pagination.limit,
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+
+  const countPromise = prisma.user.count({
+    where: { posts: {} }
+  })
+
+  const [authors, count] = await prisma.$transaction([
+    authorsPromise,
+    countPromise
+  ])
+
+  return {
+    authors,
+    totalItems: count,
+    pagination
+  }
 }
