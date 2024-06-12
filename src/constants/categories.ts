ad099148c78fa79d1d2e211416afe2fed81eaecb
hecsolmay <hecsolmay@gmail.com>
2024-06-11 23:57:32 -0600

    add categories

---
+++ src/constants/categories.ts
@@ -0,0 +1,16 @@
+export const categories = [
+  { id: 'tecnologia', name: 'Tecnología' },
+  { id: 'negocios-y-finanzas', name: 'Negocios y Finanzas' },
+  { id: 'salud-y-bienestar', name: 'Salud y Bienestar' },
+  { id: 'estilo-de-vida', name: 'Estilo de Vida' },
+  { id: 'cultura-y-entretenimiento', name: 'Cultura y Entretenimiento' },
+  { id: 'educacion-y-aprendizaje', name: 'Educación y Aprendizaje' },
+  { id: 'deportes', name: 'Deportes' },
+  { id: 'comida-y-bebida', name: 'Comida y Bebida' },
+  { id: 'ciencia-y-tecnologia', name: 'Ciencia y Tecnología' },
+  { id: 'opinion-y-editorial', name: 'Opinión y Editorial' },
+  { id: 'hobbies-y-pasatiempos', name: 'Hobbies y Pasatiempos' },
+  { id: 'noticias-y-actualidad', name: 'Noticias y Actualidad' },
+  { id: 'familia-y-crianza', name: 'Familia y Crianza' },
+  { id: 'automoviles-y-transporte', name: 'Automóviles y Transporte' }
+] as const
