5830f67684e65395d5508b6848a20c1e313603ee
hecsolmay <hecsolmay@gmail.com>
2024-06-12 11:14:11 -0600

    add hasError Prop on Input

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -4,9 +4,9 @@ import { cn } from '@/utils/cn'
 export default function Input (
   { register, ...props }: CommonInputProps
 ) {
-  const hasError = Boolean(props.error)
 
-  const { className, error, ...rest } = props
+  const { className, error, hasError = false,  ...rest } = props
+  const isError = Boolean(error) || hasError
 
   return (
     <>
@@ -15,8 +15,8 @@ export default function Input (
           `h-9 w-full appearance-none rounded-md border border-contrast bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 
           file:bg-transparent file:text-sm file:font-medium 
           focus:outline-primary focus:ring-accent focus-visible:outline-primary focus-visible:ring-1 
-          disabled:cursor-not-allowed disabled:opacity-50`,
-          hasError && 'animate-error-shake border-red-500 focus:outline-red-500 focus:ring-red-500 focus-visible:outline-red-500 focus-visible:ring-red-500',
+          disabled:cursor-not-allowed disabled:opacity-50 disabled:pointer-events-none`,
+          isError && 'animate-error-shake border-red-500 focus:outline-red-500 focus:ring-red-500 focus-visible:outline-red-500 focus-visible:ring-red-500',
           className
         )}
         {...rest}
